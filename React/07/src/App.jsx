80634c7f583bbe369720260a383a926f743761dd
Quiatus <Quiatus@gmail.com>
2025-12-09 13:33:03 +0100

    add new task

--- React/07/src/App.jsx
+++ React/07/src/App.jsx
@@ -10,21 +10,17 @@ function App() {
     selectedProjectId: undefined,
     projects: []
   })
-
-  const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId)
-
-  let content = <SelectedProject project={selectedProject} />
-
+  
   function handleClickAddProject() {
     setProjectsState(prevState => {
       return {
         ...prevState,
         selectedProjectId: null, 
-
+        
       }
     })
   }
-
+  
   function handleCancel() {
     setProjectsState(prevState => {
       return {
@@ -33,7 +29,7 @@ function App() {
       }
     })
   }
-
+  
   function handleAddProject(projectData) {
     setProjectsState(prevState => {
       const projectId =  Math.random()
@@ -48,7 +44,7 @@ function App() {
       }
     })
   }
-
+  
   function handleSelectProject(id) {
     setProjectsState(prevState => {
       return {
@@ -57,7 +53,21 @@ function App() {
       }
     })
   }
-
+  
+  function handleDeleteProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined, 
+        projects: prevState.projects.filter((project) => project.id !== prevState.selectedProjectId)
+      }
+    })
+  }
+  
+  const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId)
+  
+  let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject} />
+  
   if (projectsState.selectedProjectId === null) {
     content = <NewProject onAdd={handleAddProject} onCancel={handleCancel}/>
   } else if (projectsState.selectedProjectId === undefined) {
