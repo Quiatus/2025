eaa204b139fe2e91f7e795adaedfaec4c4005304
Quiatus <Quiatus@gmail.com>
2025-12-20 13:56:28 +0100

    add quiz logic

--- React/10/src/components/Quiz.jsx
+++ React/10/src/components/Quiz.jsx
@@ -1,12 +1,29 @@
 import { useState } from "react"
 
+import QESTIONS from '../questions.js'
+
 export default function Quiz() {
-  const [activeQuestion, setActiveQuestion] = useState(0)
   const [userAnswers, setUserAnswers] = useState([])
 
+  const activeQuestiuonIndex = userAnswers.length
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevState) => {
+      return [...prevState, selectedAnswer]
+    })
+  }
+
   return (
-    <div>
-      Question
+    <div id="quiz">
+      <div id="question">
+        <h2>{QESTIONS[activeQuestiuonIndex].text}</h2>
+        <ul id="answers">
+          {QESTIONS[activeQuestiuonIndex].answers.map(answer => 
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+            </li>)}
+        </ul>
+      </div>
     </div>
   )
 }
