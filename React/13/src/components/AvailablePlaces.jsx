4da436870492eda49a8da92694bbdbd0138950a4
Quiatus <Quiatus@gmail.com>
2026-01-13 20:34:27 +0100

    add http fetch

--- React/13/src/components/AvailablePlaces.jsx
+++ React/13/src/components/AvailablePlaces.jsx
@@ -1,10 +1,28 @@
+import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false)
+  const [availablePlaces, setAvailablePlaces] = useState([])
+
+  useEffect(() => {
+    setIsFetching(true)
+    async function fetchPlaces() {
+      const response = await fetch('http://localhost:3000/places')
+      const data = await response.json()
+      setAvailablePlaces(data.places)
+      setIsFetching(false)
+    }
+
+    fetchPlaces()
+  }, [])
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Loading places data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
