aa1a990ed2e53eabd36942ae7cb91bedf6ffcbbd
Quiatus <Quiatus@gmail.com>
2026-01-20 13:19:52 +0100

    add validation on submit

--- React/14/src/components/Login.jsx
+++ React/14/src/components/Login.jsx
@@ -1,6 +1,8 @@
 import { useRef, useState } from "react";
 
 export default function Login() {
+  const [emailIsIvalid, setEmailIsInvalid] = useState(false)
+
   const email = useRef()
   const password = useRef()
 
@@ -9,6 +11,15 @@ export default function Login() {
 
     const enteredEmail = email.current.value
     const enteredPassword = password.current.value
+
+    const emailIsValid = enteredEmail.includes('@')    
+
+    if (!emailIsValid) {
+      setEmailIsInvalid(true)
+      return
+    } 
+
+    setEmailIsInvalid(false)
   }
 
   return (
@@ -19,6 +30,7 @@ export default function Login() {
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" ref={email}/>
+          <div className="control-error">{emailIsIvalid && <p>Please enter a vaild email.</p>}</div>
         </div>
 
         <div className="control no-margin">
