3b5febb2051dc157a00980e0b3db30ee96710275
Quiatus <Quiatus@gmail.com>
2026-01-31 14:05:49 +0100

    add user context

--- React/17/src/components/Cart.jsx
+++ React/17/src/components/Cart.jsx
@@ -3,20 +3,27 @@ import Modal from "./UI/Modal";
 import Button from './UI/Button'
 import CartContext from "../store/CartContext";
 import { currencyFormatter } from '../utils/formatting'
+import UserProgressContext from "../store/UserProgressContext";
 
 export default function Cart() {
   const cartCtx = useContext(CartContext)
-  const cartTotal = cartCtx.items.reduce((totalPrice, item) => totalPrice + ite.price * item.quantity, 0)
+  const userProgressCtx = useContext(UserProgressContext)
 
-  return <Modal className="cart">
+  const cartTotal = cartCtx.items.reduce((totalPrice, item) => totalPrice + item.price * item.quantity, 0)
+
+  function handleCloseCart() {
+    userProgressCtx.hideCart()
+  }
+
+  return <Modal className="cart" open={userProgressCtx.progress === 'cart'}>
     <h2>Your Cart</h2>
     <ul>
       {cartCtx.items.map(item => <li key={item.id}>{item.name} - {item.quantity}</li>)}
     </ul>
     <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
     <p className="modal-actions">
-      <Button textOnly>Close</Button>
-      <Button>Checkout</Button>
+      <Button textOnly onClick={handleCloseCart}>Close</Button>
+      <Button onClick={handleCloseCart}>Checkout</Button>
     </p>
   </Modal>
 }
