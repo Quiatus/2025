5742a0a34b7b5ab556aebdd94a9e41f0b09a0b52
Quiatus <Quiatus@gmail.com>
2026-02-03 15:08:33 +0100

    add checkout form

--- React/17/src/components/Cart.jsx
+++ React/17/src/components/Cart.jsx
@@ -16,7 +16,11 @@ export default function Cart() {
     userProgressCtx.hideCart()
   }
 
-  return <Modal className="cart" open={userProgressCtx.progress === 'cart'}>
+  function handleCheckout() {
+    userProgressCtx.showCheckout()
+  }
+
+  return <Modal className="cart" open={userProgressCtx.progress === 'cart'} onClose={userProgressCtx.progress === 'cart' ? handleCloseCart : null}>
     <h2>Your Cart</h2>
     <ul>
       {cartCtx.items.map(item => 
@@ -30,7 +34,7 @@ export default function Cart() {
     <p className="cart-total">Total: {currencyFormatter.format(cartTotal)}</p>
     <p className="modal-actions">
       <Button textOnly onClick={handleCloseCart}>Close</Button>
-      <Button onClick={handleCloseCart} disabled={!cartCtx.items.length}>Checkout</Button>
+      <Button onClick={handleCheckout} disabled={!cartCtx.items.length}>Checkout</Button>
     </p>
   </Modal>
 }
