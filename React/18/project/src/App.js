eb9593ef79c59ecc749d1619c78279d65edce7b1
Quiatus <Quiatus@gmail.com>
2026-02-09 11:41:53 +0100

    add auth

--- React/18/project/src/App.js
+++ React/18/project/src/App.js
@@ -1,7 +1,17 @@
 import Counter from './components/Counter';
+import Auth from './components/Auth';
+import UserProfile from './components/UserProfile';
+import Header from './components/Header';
+import { useSelector } from 'react-redux'
 
 export default function App() {
+  const isLoggedIn = useSelector(state => state.auth.isAuthenticated)
+
   return (
-    <Counter />
+    <>
+      <Header />
+      {isLoggedIn ? <UserProfile /> : <Auth />}
+      <Counter />
+    </>
   );
 }
--- React/18/project/src/store/index.js
+++ React/18/project/src/store/index.js
@@ -1,33 +1,12 @@
-import { createSlice, configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from './counter'
+import authReducer from './auth'
 
-const initState = {
-  counter: 0,
-  showCounter: true
-}
-
-const counterSlice = createSlice({
-  name: 'counter',
-  initialState: initState,
-  reducers: {
-    increment(state) {
-      state.counter++
-    },
-    decrement(state) {
-      state.counter--
-    },
-    increase(state, action) {
-      state.counter += action.payload
-    },
-    toggle(state) {
-      state.showCounter = !state.showCounter
-    }
+const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+    auth: authReducer
   }
 })
 
-
-const store = configureStore({
-  reducer: counterSlice.reducer
-})
-
-export const counterActions = counterSlice.actions
 export default store
