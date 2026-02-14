6dffb875bf085ed197227d86c0b24f4c1148e31c
Quiatus <Quiatus@gmail.com>
2026-02-14 15:30:47 +0100

    implement new event

--- React/21/src/router.js
+++ React/21/src/router.js
@@ -2,8 +2,8 @@ import { createBrowserRouter } from 'react-router-dom'
 import HomePage from './pages/HomePage';
 import EventsPage, { loader as eventLoader } from './pages/EventsPage';
 import EditEventPage from './pages/EditEventPage';
-import EventDetailPage, { loader as eventDetailLoader } from './pages/EventDetailPage';
-import NewEventPage from './pages/NewEventPage';
+import EventDetailPage, { loader as eventDetailLoader, action as deleteEventAction } from './pages/EventDetailPage';
+import NewEventPage, { action as newEventAction } from './pages/NewEventPage';
 import Root from './pages/Root';
 import EventsRoot from './pages/EventsRoot';
 import Error from './pages/Error';
@@ -17,10 +17,10 @@ export const router = createBrowserRouter([
       { path: 'events', element: <EventsRoot />, children: [
         { index: true, element: <EventsPage />, loader: eventLoader },
         { path: ':eventId', id: 'event-detail', loader: eventDetailLoader, children: [
-          { index: true, element: <EventDetailPage /> },
+          { index: true, element: <EventDetailPage />, action: deleteEventAction },
           { path: 'edit', element: <EditEventPage /> },
         ]},
-        { path: 'new', element: <NewEventPage /> },
+        { path: 'new', element: <NewEventPage />, action: newEventAction },
       ]},
     ] 
   },
