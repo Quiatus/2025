d490ad75993993b62621bd7d63e0c706cbd74b7c
Quiatus <Quiatus@gmail.com>
2026-02-15 21:48:36 +0100

    update path

--- React/17/src/components/UI/Button.jsx
+++
@@ -1,9 +0,0 @@
-export default function Button({ children, textOnly, disabled=false, className, ...props }) {
-  const cssClasses = textOnly ? `text-button ${className}` : `button ${className}`
-
-  return (
-    <button disabled={disabled} className={cssClasses} {...props}>
-      {children}
-    </button>
-  )
-}
