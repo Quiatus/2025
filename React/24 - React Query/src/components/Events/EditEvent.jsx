baa835e547d85f8a2335c6979da96a61c66136c3
Quiatus <Quiatus@gmail.com>
2026-02-26 15:43:58 +0100

    add optimistic update

--- React/24 - React Query/src/components/Events/EditEvent.jsx
+++ React/24 - React Query/src/components/Events/EditEvent.jsx
@@ -1,27 +1,79 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { useMutation, useQuery } from '@tanstack/react-query';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
+import { fetchEvent, queryClient, updateEvent } from '../../util/http.js';
+import LoadingIndicator from '../UI/LoadingIndicator.jsx'
+import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function EditEvent() {
   const navigate = useNavigate();
+  const params = useParams()
 
-  function handleSubmit(formData) {}
+  const { data, isPeding, isError, error} = useQuery({
+    queryKey: ['events', params.id],
+    queryFn: ({signal}) => fetchEvent({ id: params.id, signal })
+  })
+
+  const { mutate } = useMutation({
+    mutationFn: updateEvent,
+    onMutate: async (data) => {
+      const updatedEvent = data.event
+      await queryClient.cancelQueries({ queryKey: ['events', params.id] })
+      const previousEvent = queryClient.getQueryData(['events', params.id])
+      queryClient.setQueryData(['events', params.id], updatedEvent)
+
+      return { previousEvent }
+    },
+    onError: (error, data, context) => {
+      queryClient.setQueryData(['events', params.id], context.previousEvent)
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries(['events', params.id])
+    }
+  })
+
+  function handleSubmit(formData) {
+    mutate({ id: params.id, event: formData })
+    navigate('../')
+  }
 
   function handleClose() {
     navigate('../');
   }
 
+  let content
+
+  if (isPeding) {
+    content = <div className='center'>
+      <LoadingIndicator />
+    </div>
+  }
+
+  if (isError) {
+    content = <>
+      <ErrorBlock title="Failed to load event" message={error.info?.message || 'Please try again' }/>
+      <div className="form-action">
+        <Link to="../" className='button'>Okay</Link>
+      </div>
+    </>
+  }
+
+  if (data) {
+    content = <EventForm inputData={data} onSubmit={handleSubmit}>
+      <Link to="../" className="button-text">
+        Cancel
+      </Link>
+      <button type="submit" className="button">
+        Update
+      </button>
+    </EventForm>
+  }
+
   return (
     <Modal onClose={handleClose}>
-      <EventForm inputData={null} onSubmit={handleSubmit}>
-        <Link to="../" className="button-text">
-          Cancel
-        </Link>
-        <button type="submit" className="button">
-          Update
-        </button>
-      </EventForm>
+      {content}
     </Modal>
   );
 }
