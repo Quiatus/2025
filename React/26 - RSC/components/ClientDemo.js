a67447c8dfc4aa252c589836318dd50f707a7a08
Quiatus <Quiatus@gmail.com>
2026-03-13 15:07:49 +0100

    add rsc

---
+++ React/26 - RSC/components/ClientDemo.js
@@ -0,0 +1,22 @@
+'use client'
+
+import { useState } from "react";
+
+export default function ClientDemo({ children }) {
+  const [count, setCount] = useState(0)
+
+  console.log('ClientDemo rendered');
+  return (
+    <div className='client-cmp'>
+      <h2>A React Client Component</h2>
+      <p>
+        Will be rendered on the client <strong>AND</strong> the server.
+      </p>
+      <p className="btnbox">
+        <button onClick={() => setCount(prev => prev + 1)}>+</button>
+        <strong>{count}</strong>
+      </p>
+      {children}
+    </div>
+  );
+}
