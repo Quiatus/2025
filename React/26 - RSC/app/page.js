8da1066d49675c50659360b42ffa5bf9e994f746
Quiatus <Quiatus@gmail.com>
2026-03-15 15:28:31 +0100

    rsc

--- React/26 - RSC/app/page.js
+++ React/26 - RSC/app/page.js
@@ -1,11 +1,29 @@
 import ClientDemo from "@/components/ClientDemo";
+import DataFetchingDemo from "@/components/DataFetchingDemo";
 import RSCDemo from "@/components/RSCDemo";
+import ServerActionsDemo from "@/components/ServerActionsDemo";
+import UsePromiseDemo from "@/components/UsePromisesDemo";
 
-export default function Home() {
+import { Suspense } from "react";
+import fs from 'node:fs/promises';
+
+export default async function Home() {
+  const fetchUsersPromise = new Promise(res => 
+    setTimeout(async () => {
+      const data = await fs.readFile('dummy-db.json', 'utf-8');
+      const users = JSON.parse(data);
+      res(users)
+  }, 2000))
+
   return (
     <main>
       <RSCDemo />
       <ClientDemo />
+      <DataFetchingDemo />
+      <ServerActionsDemo />
+      <Suspense fallback={<p>Loading...</p>}>
+        <UsePromiseDemo usersPromise={fetchUsersPromise} />
+      </Suspense>
     </main>
   );
 }
