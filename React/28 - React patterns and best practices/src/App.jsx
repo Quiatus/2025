8c33c6df14e07ef0dff7d60a4035218437e7dcf0
Quiatus <Quiatus@gmail.com>
2026-03-17 15:49:15 +0100

    add compund comps

--- React/28 - React patterns and best practices/src/App.jsx
+++ React/28 - React patterns and best practices/src/App.jsx
@@ -1,5 +1,25 @@
+import Accordion from "./components/Accordion/Accordion";
+
 function App() {
-  return <h1>React Patterns & Practices</h1>;
+  return <main>
+    <section>
+      <h2>Why us?</h2>
+      <Accordion className="accordion">
+        <Accordion.Item id="1" className="accordion-item" title="We have 20 years of experience">
+          <article>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
+            <p>Animi ea quis quia expedita beatae, id laudantium nostrum soluta saepe voluptatem asperiores tenetur aspernatur perferendis aut repellendus nobis et? Nostrum, asperiores?</p>
+          </article>
+        </Accordion.Item>
+        <Accordion.Item id="2" className="accordion-item" title="We work with local guides.">
+          <article>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
+            <p>Animi ea quis quia expedita beatae, id laudantium nostrum soluta saepe voluptatem asperiores tenetur aspernatur perferendis aut repellendus nobis et? Nostrum, asperiores?</p>
+          </article>
+        </Accordion.Item>
+      </Accordion>
+    </section>
+  </main>;
 }
 
 export default App;
