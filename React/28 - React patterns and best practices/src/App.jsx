00dc20e7693f99b9b644387d79d657bf65fcf183
Quiatus <Quiatus@gmail.com>
2026-03-19 13:39:44 +0100

    add comps

--- React/28 - React patterns and best practices/src/App.jsx
+++ React/28 - React patterns and best practices/src/App.jsx
@@ -5,21 +5,32 @@ function App() {
     <section>
       <h2>Why us?</h2>
       <Accordion className="accordion">
-        <Accordion.Item id="1" className="accordion-item" title="We have 20 years of experience">
-          <article>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
-            <p>Animi ea quis quia expedita beatae, id laudantium nostrum soluta saepe voluptatem asperiores tenetur aspernatur perferendis aut repellendus nobis et? Nostrum, asperiores?</p>
-          </article>
+        <Accordion.Item id="1" className="accordion-item">
+          <Accordion.Title className="accordion-item-title" id="1">
+            We have 20 years of experience
+          </Accordion.Title>
+          <Accordion.Content className="accordion-item-content" id="1">
+            <article>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
+              <p>Animi ea quis quia expedita beatae, id laudantium nostrum soluta saepe voluptatem asperiores tenetur aspernatur perferendis aut repellendus nobis et? Nostrum, asperiores?</p>
+            </article>
+          </Accordion.Content>
         </Accordion.Item>
-        <Accordion.Item id="2" className="accordion-item" title="We work with local guides.">
-          <article>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
-            <p>Animi ea quis quia expedita beatae, id laudantium nostrum soluta saepe voluptatem asperiores tenetur aspernatur perferendis aut repellendus nobis et? Nostrum, asperiores?</p>
-          </article>
+
+        <Accordion.Item id="2" className="accordion-item">
+          <Accordion.Title className="accordion-item-title" id="2">
+            We work with local guides.
+          </Accordion.Title>
+          <Accordion.Content className="accordion-item-content" id="2">
+            <article>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
+              <p>Animi ea quis quia expedita beatae, id laudantium nostrum soluta saepe voluptatem asperiores tenetur aspernatur perferendis aut repellendus nobis et? Nostrum, asperiores?</p>
+            </article>
+          </Accordion.Content>
         </Accordion.Item>
       </Accordion>
     </section>
-  </main>;
+  </main>
 }
 
 export default App;
--- React/28 - React patterns and best practices/src/components/Accordion/AccordionItem.jsx
+++ React/28 - React patterns and best practices/src/components/Accordion/AccordionItem.jsx
@@ -1,14 +1,19 @@
-import { useAccordionContext } from "./Accordion"
+import { createContext, useContext } from "react"
 
-export default function AccordionItem({ id, className, title, children }) {
-  const { openItemId, toggleItem } = useAccordionContext()
+const AccordionItemContext = createContext()
 
-  const isOpen = openItemId === id
+export function useAccordionItemContext() {
+  const ctx = useContext(AccordionItemContext)
 
-  return (
-    <li className={className}>
-      <h3 onClick={() => toggleItem(id)}>{title}</h3>
-      <div className={isOpen ? 'accordion-item-content open': 'accordion-item-content'}>{children}</div>
-    </li>
-  )
+  if (!ctx) {
+    throw new Error('Accordion Item related components must be wrapped by <Accordion.Item>')
+  }
+
+  return ctx
+}
+
+export default function AccordionItem({ id, className, children }) {
+  return <AccordionItemContext.Provider value={id}>
+    <li className={className}>{children}</li>
+  </AccordionItemContext.Provider>
 }
