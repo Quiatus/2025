0fccf9e594f8e0b965e028b013993471d25addc9
Quiatus <Quiatus@gmail.com>
2026-03-21 14:58:46 +0100

    add list

--- React/30 - React + TS/react-ts/src/App.tsx
+++ React/30 - React + TS/react-ts/src/App.tsx
@@ -1,15 +1,20 @@
+import { useState } from 'react';
 import './App.css';
+import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './models/todo';
 
 export default function App() {
-  const todos = [
-    new Todo('Learn React'),
-    new Todo('Learn TS')
-  ]
+  const [ todos, setTodos] = useState<Todo[]>([])
+
+  function addTodo(todoText: string) {
+    const newTodo = new Todo(todoText)
+    setTodos([...todos, newTodo])
+  }
 
   return (
     <div>
+      <NewTodo onAddTodo={addTodo}/>
       <Todos items={todos} />
     </div>
   );
--- React/30 - React + TS/react-ts/src/components/NewTodo.tsx
+++ React/30 - React + TS/react-ts/src/components/NewTodo.tsx
@@ -1,17 +1,18 @@
 import React, { useRef } from "react"
 
-const NewTodo = () => {
+const NewTodo: React.FC<{ onAddTodo: (text: string ) => void }> = (props) => {
   const todoText = useRef<HTMLInputElement>(null)
 
   function submit(e: React.FormEvent) {
     e.preventDefault()
-    const enteredText = todoText.current?.value
+    const enteredText = todoText.current!.value
 
     if (enteredText?.trim().length === 0) {
       return
     }
 
-    
+    props.onAddTodo(enteredText)
+    todoText.current!.value = ''
   }
 
   return <form onSubmit={submit}>
