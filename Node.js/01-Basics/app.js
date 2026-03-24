79f9e00851a3b33cd69a916610ee4b66189454ad
Quiatus <Quiatus@gmail.com>
2026-03-24 15:18:27 +0100

    node basic

---
+++ Node.js/01-Basics/app.js
@@ -0,0 +1,40 @@
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+  const url = req.url
+  const method = req.method
+  
+  if (url === '/') {
+    res.write('<html>')
+    res.write('<head><title>Message</title></head>')
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
+    res.write('</head>')
+    return res.end()
+  }
+
+  if (url === '/message' && method === 'POST') {
+    const body = []
+    req.on('data', (chunk) => {
+      console.log(chunk)
+      body.push(chunk)
+    })
+    req.on('end', () => {
+      const paresedBody = Buffer.concat(body).toString()
+      const message = paresedBody.split('=')[1]
+      fs.writeFileSync('message.txt', message)
+    })
+    res.statusCode = 302
+    res.setHeader('Location', '/')
+    return res.end()
+  }
+  
+  res.setHeader('Content-Type', 'text/html')
+  res.write('<html>')
+  res.write('<head><title>Node</title></head>')
+  res.write('<body>test</body>')
+  res.write('</head>')
+  res.end()
+})
+
+server.listen(3000)
