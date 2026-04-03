b16feb7f5d9ed6116f03e684cc9fafe3087e0d59
Quiatus <Quiatus@gmail.com>
2026-04-03 13:31:43 +0200

    complete

--- React+TS/01-basic/src/components/CourseGoalList.tsx
+++ React+TS/01-basic/src/components/CourseGoalList.tsx
@@ -1,5 +1,7 @@
 import CourseGoal from ".//CourseGoal.tsx";
+import InfoBox from "./InfoBox.tsx";
 import { type CourseGoal as CourseGoalType } from "../App.tsx";
+import type { ReactNode } from "react";
 
 type CourseGoalList = {
   goals: CourseGoalType[];
@@ -7,11 +9,25 @@ type CourseGoalList = {
 }
 
 export default function CourseGoalList({goals, onDelete }: CourseGoalList) {
+
+  if (goals.length === 0) {
+    return <InfoBox mode="hint">No goals yet. Start adding some!</InfoBox>
+  }
+
+  let warningBox: ReactNode
+
+  if (goals.length >= 4) {
+    warningBox = <InfoBox mode="warning" severity="low">Too many goals. Don't put too much on your plate!</InfoBox>
+  }
+
   return (
+    <>
+    {warningBox}
     <ul>
       {goals.map(goal => 
         <li key={goal.id}><CourseGoal id={goal.id} title={goal.title} onDelete={onDelete}>{goal.description}</CourseGoal></li>
       )}
     </ul>
+    </>
   )
 }
