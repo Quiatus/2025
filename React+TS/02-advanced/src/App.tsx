d5d164d3797aafb7131ddd3b6623a3755f635b00
Quiatus <Quiatus@gmail.com>
2026-04-03 16:36:24 +0200

    add polymorphic component

--- React+TS/02-advanced/src/App.tsx
+++ React+TS/02-advanced/src/App.tsx
@@ -1,12 +1,14 @@
-import Input from "./components/Input";
+// import Input from "./components/Input";
 import Button from "./components/Button";
+import Container from "./components/Container";
 
 function App() {
   return <main>
-    <Input id="name" label="Name" type="text"/>
+    {/* <Input id="name" label="Name" type="text"/>
     <Input id="age" label="Age" type="number" min={0}/>
     <Button el="button">Click me!</Button>
-    <Button el="anchor" href="#">Click me too!</Button>
+    <Button el="anchor" href="#">Click me too!</Button> */}
+    <Container as={Button} >Click </Container>
   </main>;
 }
 
