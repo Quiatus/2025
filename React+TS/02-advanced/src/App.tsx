32a416e41f8c60043986ea2aaceb50f8826cfc6e
Quiatus <Quiatus@gmail.com>
2026-04-05 20:43:44 +0200

    modify project to add timers

--- React+TS/02-advanced/src/App.tsx
+++ React+TS/02-advanced/src/App.tsx
@@ -1,39 +1,50 @@
-import Input from "./components/Input";
-import Button from "./components/Button";
-import { useRef } from "react";
-import Form, { type FormHandler } from "./components/Form";
+// import Input from "./components/UI/Input";
+// import Button from "./components/UI/Button";
+// import { useRef } from "react";
+// import Form, { type FormHandler } from "./components/UI/Form";
+import Header from "./components/Header";
+import AddTimer from "./components/AddTimer";
+import Timers from "./components/Timers";
 // import Container from "./components/Container";
 
 function App() {
   //const input = useRef<HTMLInputElement>(null)
 
-  const customForm = useRef<FormHandler>(null)
+  // const customForm = useRef<FormHandler>(null)
 
-  function saveHandler(data: unknown) {
-    const extractedData = data as {
-      name: string;
-      age: string
-    }
+  // function saveHandler(data: unknown) {
+  //   const extractedData = data as {
+  //     name: string;
+  //     age: string
+  //   }
 
-    console.log(extractedData)
-    customForm.current?.clear()
-  }
+  //   console.log(extractedData)
+  //   customForm.current?.clear()
+  // }
 
-  return <main>
-    {/* <Input id="name" label="Name" type="text" ref={input}/>
-    <Input id="age" label="Age" type="number" min={0}/> 
-    <Button el="button">btn</Button>
-    <Button el="anchor" href="#">Click me too!</Button> 
-    <Container as={Button} >Click </Container> */}
+  return (
+    <>
+      <Header/>
+      <main>
+      {/* <Input id="name" label="Name" type="text" ref={input}/>
+      <Input id="age" label="Age" type="number" min={0}/> 
+      <Button el="button">btn</Button>
+      <Button el="anchor" href="#">Click me too!</Button> 
+      <Container as={Button} >Click </Container> */}
 
-    <Form onSave={saveHandler} ref={customForm}>
-      <Input type="text" label="Name" id="name" />
-      <Input type="number" label="Age" id="age" />
-      <p>
+      {/* <Form onSave={saveHandler} ref={customForm}>
+        <Input type="text" label="Name" id="name" />
+        <Input type="number" label="Age" id="age" />
+        <p>
         <Button>Save</Button>
-      </p>
-    </Form>
-  </main>;
+        </p>
+        </Form> */}
+
+        <AddTimer />
+        <Timers />
+      </main>
+    </>
+  )
 }
 
 export default App;
