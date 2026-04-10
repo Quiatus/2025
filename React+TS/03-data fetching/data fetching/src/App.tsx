7c74fe9a22c7fdfc70446028df7534a697ea7e86
Quiatus <Quiatus@gmail.com>
2026-04-10 15:14:12 +0200

    add data fetching

--- React+TS/03-data fetching/data fetching/src/App.tsx
+++ React+TS/03-data fetching/data fetching/src/App.tsx
@@ -1,5 +1,45 @@
+import { useEffect, useState, type ReactNode } from "react";
+import BlogPosts, {type BlogPost} from "./components/BlogPosts";
+import { get } from "./util/http";
+import img from './assets/data-fetching.png'
+
+type RawPostsData = {
+  id: number;
+  userId: number;
+  title: string;
+  body: string
+}
+
 function App() {
-  return <h1>Data Fetching!</h1>;
+  const [posts, setPosts] = useState<BlogPost[]>()
+  
+  useEffect(() => {
+    async function fetchPosts() {
+      const data = await get('https://jsonplaceholder.typicode.com/posts') as RawPostsData[]
+
+      const blogPosts: BlogPost[] = data.map(post => {
+        return {
+          id: post.id,
+          title: post.title,
+          text: post.body
+        }
+      })
+
+      setPosts(blogPosts)
+    }
+
+    fetchPosts()
+  }, [])
+
+  let content: ReactNode
+
+  if (posts) {
+    content = <BlogPosts posts={posts}/>
+  }
+  return <main>
+    <img src={img} alt="data" />
+    {content}
+  </main>;
 }
 
 export default App;
