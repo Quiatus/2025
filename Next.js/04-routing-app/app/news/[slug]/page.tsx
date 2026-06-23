866ca8f6461620868096bbd2a02a8c09752594d5
Quiatus <Quiatus@gmail.com>
2026-06-23 16:00:15 +0200

    add layout

---
+++ Next.js/04-routing-app/app/news/[slug]/page.tsx
@@ -0,0 +1,21 @@
+import { DUMMY_NEWS } from "@/dummy-news"
+
+type Props = {
+  params: Promise<{ slug: string }>
+}
+
+export default async function NewsArticle({ params }: Props) {
+  const { slug } = await params
+  const newsItem = DUMMY_NEWS.find(newsItem => newsItem.slug === slug)
+
+  return (
+    <article className="news-article">
+      <header>
+        <img src={`/images/news/${newsItem!.image}`} alt={newsItem!.title} />
+        <h1>{newsItem!.title}</h1>
+        <time dateTime={newsItem!.date}>{newsItem!.date}</time>
+      </header>
+      <p>{newsItem?.content}</p>
+    </article>
+  )
+}
