080a6f6b48f3c9ff7230f647edfb9841f8c55283
Quiatus <Quiatus@gmail.com>
2026-06-25 14:04:56 +0200

    add client nav link

---
+++ Next.js/04-routing-app/app/components/nav-link.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { ReactNode } from 'react'
+
+interface NavLinkProps {
+  children: ReactNode;
+  href: string;
+}
+
+export default function NavLink({href, children}: NavLinkProps) {
+  const path = usePathname()
+
+  return (
+    <Link href={href} className={path.startsWith(href) ? 'active' : undefined}>{children}</Link>
+  )
+}
