ae3f1853653267c54ae0386ca64694e656e7de22
Quiatus <Quiatus@gmail.com>
2026-09-08 20:10:39 +0200

    add courses

--- FullStackOpen/nodejs/00-basics/index.js
+++ FullStackOpen/nodejs/00-basics/index.js
@@ -1,5 +1,27 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
+
+const password = process.argv[2]
+const url = `mongodb+srv://quiatus:${password}@cluster0.hs92i01.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url, { family: 4 })
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Note = mongoose.model('Note', noteSchema)
 
 app.use(express.json())
 app.use(express.static('dist'))
@@ -27,7 +49,9 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
