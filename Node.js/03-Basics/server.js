3d0328e44360ff5779e2ade1293d1981dd12ddba
Quiatus <Quiatus@gmail.com>
2026-09-14 21:11:50 +0200

    node stuff

--- Node.js/03-Basics/server.js
+++ Node.js/03-Basics/server.js
@@ -1,7 +1,21 @@
 import http from 'http'
+import fs from 'fs'
 
 const server = http.createServer((req, res) => {
-  console.log('request')
+  console.log(req.url, req.method)
+
+  res.setHeader('Content_type', 'text/html')
+  
+  fs.readFile('./page/home.html', (err, data) => {
+    if (err) {
+      console.log(err)
+      res.end()
+    } else {
+      res.write(data)
+      res.end()
+    }
+  })
+
 })
 
 server.listen(3000, 'localhost', () => {
