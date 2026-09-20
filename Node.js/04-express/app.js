e4a800bb45742f0b151157f0fd6a6d71f5b53c6f
Quiatus <Quiatus@gmail.com>
2026-09-20 10:24:27 +0200

    add ejs

--- Node.js/04-express/app.js
+++ Node.js/04-express/app.js
@@ -2,22 +2,23 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
-app.use(express.static(path.join(__dirname, 'page')))
 
-app.listen(3000, () => console.log('Server running'))
+app.use(express.static(path.join(__dirname, 'page')))
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'))
 })
 
 app.get('/about', (req, res) => {
-  res.sendFile('./page/about.html', { root: __dirname})
+  res.sendFile(path.join(__dirname, 'page', 'about.html'))
 })
 
 app.get('/about-us', (req, res) => {
-  res.redirect('/about', { root: __dirname})
+  res.redirect('/about')
 })
 
 app.use((req, res) => {
-  res.status(404).sendFile('./page/404.html', { root: __dirname})
+  res.status(404).sendFile(path.join(__dirname, 'page', '404.html'))
 })
+
+app.listen(3000, () => console.log('Server running on port 3000'))
