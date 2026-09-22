a396da3aa0835e205843da0c00e54c157a6a45ef
Quiatus <Quiatus@gmail.com>
2026-09-22 15:51:57 +0200

    add mongodb

--- Node.js/04-express/ejsapp.js
+++ Node.js/04-express/ejsapp.js
@@ -1,18 +1,49 @@
-const express = require('express')
+//const express = require('express')
+import express from 'express'
+import mongoose from 'mongoose'
+import { Blog } from './models/blog.js'
+import { username, password } from './local.js'
 
 const app = express()
 
+const dbURI = `mongodb+srv://${username}:${password}@cluster0.nehwedc.mongodb.net/node-blog?retryWrites=true&w=majority`
+
+async function connectDB() {
+  try {
+    await mongoose.connect(dbURI)
+    console.log('DB connected')
+    app.listen(3000, () => console.log('Server running on port 3000'))
+  } catch (err) {
+    console.log(err)
+  }
+}
 app.set('view engine', 'ejs')
 
-// app.use((req, res, next) => {
-//   console.log('request made')
-//   console.log('host: ', req.hostname)
-//   console.log('path: ', req.path)
-//   console.log('method: ', req.method)
-//   next()
-// })
-
 app.use(express.static('public'))
+
+app.get('/add-blog', async (req, res) => {
+  const blog = new Blog({
+    title: 'New Blog',
+    snippet: 'About new blog',
+    body: 'text text text text'
+  })
+
+  try {
+    const result = await blog.save()
+    res.send(result)
+  } catch (err) {
+    console.log(err)
+  }
+})
+
+app.get('/all-blogs', async (req, res) => {
+  try {
+    const blogs = await Blog.find()
+    res.send(blogs)
+  } catch (err) {
+    console.log(err)
+  }
+})
 
 app.get('/', (req, res) => {
   const blogs = [
@@ -35,4 +66,12 @@ app.use((req, res) => {
   res.status(404).render('404', { title: '404' })
 })
 
-app.listen(3000, () => console.log('Server running on port 3000'))
+connectDB()
+
+// app.use((req, res, next) => {
+//   console.log('request made')
+//   console.log('host: ', req.hostname)
+//   console.log('path: ', req.path)
+//   console.log('method: ', req.method)
+//   next()
+// })
